eaf6acefcb8aa94d49ef5ef55ce0d866e8de642e
Angel Chaidez <angelomarchaidez@gmail.com>
2023-05-30 22:16:34 -0700

    change

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 provider "aws" {
   region = var.region
+  shared_credentials_file = "$HOME/.aws/credentials"
+  profile                 = "default"
 }
+
 
 module "s3-webapp" {
   source  = "app.terraform.io/cloudgeeks/s3-webapp/aws"
