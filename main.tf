b51b309171f5def91e5aac57840efa97ab2f7bdd
Angel Chaidez <angelomarchaidez@gmail.com>
2023-05-30 21:56:44 -0700

    main

--- main.tf
+++ main.tf
@@ -11,11 +11,17 @@ provider "aws" {
   region = var.region
 }
 
+
 module "s3-webapp" {
   source  = "app.terraform.io/cloudgeeks/s3-webapp/aws"
   name   = var.name
   region = var.region
   prefix = var.prefix
   version = "1.0.0"
+}
+
+credentials "app.terraform.io" {
+  # valid user API token
+  token = "DYhVxxZ7uIuLzQ.atlasv1.yHLIpPZqafnKQtDKYkmxz0twGV5xiZ0svbSV5g6xWUVolNNetv1Izrl6SbTHqxynzGk"
 }
 
